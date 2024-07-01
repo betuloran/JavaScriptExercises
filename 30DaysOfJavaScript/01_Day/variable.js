c0d59c50fa3d6c9171b77cd779d22c098090e663
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-07-01 17:32:49 +0300

    01_Day file added

---
+++ 30DaysOfJavaScript/01_Day/variable.js
@@ -0,0 +1,23 @@
+// Declaring different variables of different data types
+let firstName = 'Betül' // first name of a person
+let lastName = 'Oran' // last name of a person
+let country = 'Turkey' // country
+let city = 'Kütahya' // capital city
+let age = 23 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Betül', //name of a person
+  job = 'software developer',
+  live = 'Turkey'
+
+  console.log(firstName)  //Betül
+  console.log(city)   //Kütahya
+ 
+  console.log(typeof 'Kütahya')  //çıktı: string
+  console.log(typeof false) //çıktı: boolean
