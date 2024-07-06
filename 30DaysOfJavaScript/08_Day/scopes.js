c6161bf8d1aa03207d3f5282e0de48ac3b5b4697
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-07-06 19:15:49 +0300

    update scope.js

--- 30DaysOfJavaScript/08_Day/scopes.js
+++ 30DaysOfJavaScript/08_Day/scopes.js
@@ -70,8 +70,11 @@ length
 [[Prototype]]
 : 
 Array(0) */
+
+// nokta kullanılarak değerlere erişiliyor.
 console.log(person.siblings[0].name) //meltem
 
+//değere köşeli parantez ve anahtar adı kullanılarak erişilebilir. örneğin telefon numarasına erişmek için yalnızca köşeli parantez yöntemini kullanıyoruz
 console.log(person['phone number']) //45094580
 
 //illa '...' şeklinde olanlara erişebiliriz diye bir şey yok name surname de de sonuç verir
@@ -81,3 +84,79 @@ console.log(person['name']) //tayfun
 let keyName='name'
 console.log(person[keyName]) //tayfun 
 
+//Nesnenin farklı özelliklerinin değerlerine erişmek için this kelimesini kullanabiliriz:
+const user = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    isMarried:false,
+    address: {
+        street: 'Heitamienkatu 16',
+        pobox: 2002,
+        city: 'Helsinki'
+      },
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+  console.log(user.getFullName()) // Asabeneh Yetayeh
+
+  //VERİLERİ DEĞİŞTİRMEK VE VERİ EKLEMEK
+user.country = 'Finland'
+user.title='developer'
+user.skills.push('Java')
+user.skills.push('SCSS')   //skills'e iki veri eklendi Java ve SCSS 
+user.isMarried = true
+user.firstName='Ali'
+
+console.log(user)
+
+
+user.getPersonInfo = function() {
+    let skillsWithoutLastOne = this.skills.splice(0, this.skills.length - 1).join(', ')
+    let lastSkill = this.skills.at(-1) //Sondan birinci(yani sonuncu ) veriyi istedik 
+  //  console.log(lastSkill) //Son veri SCSS'ti yukarıda pushlamıştık. 
+  let skills = `${skillsWithoutLastOne}, and ${lastSkill}`
+  let fullName = this.getFullName()
+  let statement = `${fullName} is a ${this.title}.\n 
+  He lives in ${this.country}.\n 
+  He teaches ${skills}.`
+  return statement
+  
+}
+console.log(user.getPersonInfo())  //SCSS (console.log(lastSkill) 'in çıktısı)
+// Ali Yetayeh is a developer.
+// He lives in Finland.
+// He teaches HTML, CSS, JavaScript, React, Node, MongoDB, Python, D3.js, Java, and SCSS.
+
+const copyUser = Object.assign({}, user)
+console.log(copyUser)
+
+//bject.keys() kullanarak nesne anahtarlarını alma ::::
+//Object.keys: Bir nesnenin anahtarlarını veya özelliklerini dizi olarak almak için kullanılır. 
+const keys = Object.keys(copyUser)
+console.log(keys) //['firstName', 'lastName', 'age', 'country', 'city', 'isMarried', 'skills', 'getFullName', 'title', 'getPersonInfo']
+
+//Object.values: Bir nesnenin değerlerini dizi olarak almak için:::
+const entries = Object.entries(copyUser)
+console.log(entries)
+
+//hasOwnProperty: Bir nesnede belirli bir anahtarın veya özelliğin mevcut olup olmadığını kontrol etmek için::
+console.log(copyUser.hasOwnProperty('name')) //false
+console.log(copyUser.hasOwnProperty('score')) //false
+
+
+//EĞER OBJENİN İÇİNDEKİLERİ DEĞİŞTİRİLMESİN İSTERSEK ::
+Object.freeze() //KULLAN
