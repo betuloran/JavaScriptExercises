64be9ab20b7e3b3d836e90c369512c0b8f528160
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-07-11 11:39:19 +0300

    main.js updated

---
+++ 30DaysOfJavaScript/09_Day/main.js
@@ -0,0 +1,23 @@
+//CALLBACK : callback, başka bir işleve parametre olarak aktarılabilen bir işlevdir. Aşağıdaki örneğe bakın:
+// bir sayının küpünü hesaplamak için bir geri çağırma(callback) fonksiyonu kullanır.Kod, bir sayının karesini 
+// alan bir fonksiyon(fonksiyon) ve bu fonksiyonu kullanarak bir sayının küpünü hesaplayan başka bir fonksiyon(cube) tanımlar.
+
+const fonksiyon = number => number ** 2   // 2 kez üssünü al yani 3*3*3
+
+function cube(callback, number) { //callback ve number fonksiyonu alıyor
+    return callback(number) * number
+}
+console.log(cube(fonksiyon, 3))
+
+//diğer kullanım şekli 1:
+//bir geri çağırma işlevi, işlevin adı herhangi bir ad olabilir
+// const callback = (n) => {
+//     return n ** 2
+//   }
+
+//  // diğer işlevi geri arama olarak alan işlev
+//   function cube(callback, n) {
+//     return callback(n) * n
+//   }
+//
+//   console.log(cube(callback, 3))
