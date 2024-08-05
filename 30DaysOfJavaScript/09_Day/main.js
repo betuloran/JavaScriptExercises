f45e381f0becbf47857f7699befce8c0b85976dd
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-08-05 21:12:50 +0300

    09_Day updated

--- 30DaysOfJavaScript/09_Day/main.js
+++ 30DaysOfJavaScript/09_Day/main.js
@@ -97,4 +97,130 @@ function sayHello() {
 setTimeout(sayHello, 2000) // it prints hello after it waits for 2 seconds.(2 saniye bekledikten sonra merhaba yazdırıyor.)
 
 
-//
+//FUNCTIONAL PROGRAMMING (FONKSİYONEL PROGRAMLAMA):
+//forEach ' in farklı kullanım şekilleri (hepsi aynı sonucu verir)
+// arr.forEach(function (element, index, arr) {
+//   console.log(index, element, arr)
+// })
+// // The above code can be written using arrow function
+// arr.forEach((element, index, arr) => {
+//   console.log(index, element, arr)
+// })
+// // The above code can be written using arrow function and explicit return
+// arr.forEach((element, index, arr) => console.log(index, element, arr))
+
+//örnek:
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+countries.forEach((element) => console.log(element.toUpperCase())) //FINLAND DENMARK SWEDEN NORWAY ICELAND
+
+//map fonksiyonu: Bir dizi öğesini yineleyin ve dizi öğelerini değiştirin.
+const  names= ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+console.log(namesToUpperCase) //['ASABENEH', 'MATHIAS', 'ELIAS', 'BROOK']
+
+//filter fonk: Filtreleme koşullarını tam olarak dolduran öğeleri filtreleyin ve yeni bir dizi döndürün.
+
+const countries2 = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+const countriesContainingLand = countries2.filter((country) =>
+  country.includes('land')
+)
+console.log(countriesContainingLand) //sonu land ile bitenleri filtreledik: finland ireland
+
+//every fonk: tüm öğelerin bir açıdan benzer olup olmadığını kontrol edin. Boolean değerini döndürür
+const names3 = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const areAllStr = names3.every((name) => typeof name === 'string') // Are all strings?
+
+console.log(areAllStr) //true
+
+//find: Koşulu karşılayan ilk öğeyi döndür
+//örn
+const ages = [24, 22, 25, 32, 35, 18]
+const age = ages.find((age) => age < 20)
+
+console.log(age) //18
+
+//örn
+const scores = [
+  { name: 'Asabeneh', score: 95 },
+  { name: 'Mathias', score: 80 },
+  { name: 'Elias', score: 50 },
+  { name: 'Martha', score: 85 },
+  { name: 'John', score: 100 },
+]
+
+const score = scores.find((user) => user.score > 80)
+console.log(score) //{ name: "Asabeneh", score: 95 }
+
+//findIndex: Koşulu karşılayan ilk öğenin konumunu döndürür
+const names5 = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const ages1 = [24, 22, 25, 32, 35, 18]
+
+const result = names5.findIndex((name) => name.length > 7)
+console.log(result) // 0 -asabeneh 0.indexte old için
+ 
+const age1 = ages1.findIndex((age) => age < 20)
+console.log(age1) // 5
+
+//some: Bazı öğelerin bir açıdan benzer olup olmadığını kontrol edin. Boole değerini döndürür
+const names6 = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const bools = [true, true, true, true]
+
+const areSomeTrue = bools.some((b) =>  b === true)
+
+console.log(areSomeTrue) //true
+
+//EVERY VE SOME FONKSİYONLARININ FARKI:
+// every() fonksiyonu
+// Dizinin tüm öğelerinin verilen koşulu karşılayıp karşılamadığını belirlemek için kullanılır.
+
+// some() fonksiyonu
+// Dizinin en az bir öğesinin sağlanan ölçütü karşılayıp karşılamadığını belirlemek için kullanılır.
+
+// every() ve some() fonksiyonunun farkı
+// Herhangi bir koşul sağlanırsa some() fonksiyonu true değerini döndürmesi, ancak bunların tümü doğruysa every() fonksiyonunun true, döndürmesidir.
+
+//SOME:
+function isnumbereven(element) {
+  return (element % 2 == 0);
+}
+
+function print() {
+  var arr = [ 4, 3, 13, 43, 58 ];
+
+  // sadece bir eleman koşulu sağlıyorsa
+  var value = arr.some(isnumbereven);
+  console.log(value);  // output true
+}
+print();
+
+//EVERY:
+function isnumbereven2(element) {
+  return (element % 2 == 0);
+}
+
+function print() {
+  var array = [ 4, 3, 13, 43, 58 ];
+
+  // bütün numaraları kontrol etme
+  var value_two = array.every(isnumbereven2);
+  console.log(value_two);// output false
+}
+
+print();
+
+//sort fonksiyonu: bir dizinin ( array ) sıralanması için kullanılan fonksiyondur.
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log(products.sort()) // ['Apple', 'Carrot', 'Coffee', 'Honey', 'Milk', 'Sugar']
+//Now the original products array  is also sorted
