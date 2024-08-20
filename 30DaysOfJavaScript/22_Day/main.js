8993333e3f799618d5a6133ec3632f3d3ad0b8a3
unknown <betuloran2@gmail.com>
2024-08-20 17:54:45 +0300

    22_Day file added

---
+++ 30DaysOfJavaScript/22_Day/main.js
@@ -0,0 +1,4 @@
+//DOM MANİPÜLASYONU : classList.add() ve classList.remove()
+// classList.add(), bir HTML elementine bir veya daha fazla CSS sınıfı eklemeyebilme özelliğini sağlarken 
+//classList.remove() ise bir HTML elementinden bir veya daha fazla CSS sınıfını kaldırabilmeyi sağlar
+
