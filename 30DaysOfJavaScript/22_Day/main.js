b5d1bc95ffb89c37332c0f4ecf0194f63438861b
unknown <betuloran2@gmail.com>
2024-08-22 19:14:06 +0300

    main.js updated

--- 30DaysOfJavaScript/22_Day/main.js
+++ 30DaysOfJavaScript/22_Day/main.js
@@ -2,3 +2,34 @@
 // classList.add(), bir HTML elementine bir veya daha fazla CSS sınıfı eklemeyebilme özelliğini sağlarken 
 //classList.remove() ise bir HTML elementinden bir veya daha fazla CSS sınıfını kaldırabilmeyi sağlar
 
+
+
+
+
+const paragraph = document.getElementById('myParagraph');
+const toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', () => {
+    if (paragraph.classList.contains('highlight')) {
+
+        paragraph.classList.remove('highlight');
+        toggleButton.textContent = 'Paragrafı Vurgula'; 
+    } else {
+    
+        paragraph.classList.add('highlight');
+        toggleButton.textContent = 'Vurgulamayı Kaldır'; 
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
