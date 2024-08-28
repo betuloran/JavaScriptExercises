3ddf76f6f16810209f5f98cc099525f05219d01c
unknown <betuloran2@gmail.com>
2024-08-28 23:49:04 +0300

    main.js updated

---
+++ udemyCourse/degiskenTanimlama/main.js
@@ -0,0 +1,6 @@
+let gun=14;
+let ay=3;
+let yil=1993;
+
+console.log(gun, ay, yil) //14 3 1993
+console.log("Betül Oran "+gun+"/"+ay+"/"+yil+" tarihinde doğmuştur.") //Betül Oran 14/3/1993 tarihinde doğmuştur.
