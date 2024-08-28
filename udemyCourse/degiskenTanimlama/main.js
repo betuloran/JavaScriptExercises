6d1382f2a93fbc9c43bd69f278b169ee03eddce9
unknown <betuloran2@gmail.com>
2024-08-28 23:58:30 +0300

    const variable added

--- udemyCourse/degiskenTanimlama/main.js
+++ udemyCourse/degiskenTanimlama/main.js
@@ -4,3 +4,12 @@ let yil=1993;
 
 console.log(gun, ay, yil) //14 3 1993
 console.log("Betül Oran "+gun+"/"+ay+"/"+yil+" tarihinde doğmuştur.") //Betül Oran 14/3/1993 tarihinde doğmuştur.
+
+gun=29;
+console.log(gun) //29 - let ile değerler değiştirilebilir
+
+const nufus=2000;
+nufus=1000;
+console.log(nufus) // Assignment to constant variable. - konsol hata verir, const ile değerler değiştirilemez
+
+//NOT: js, hata alınan koddan sonraki kodları okumaz. bu durum, önem verilmesi gereken bir noktadır.  
