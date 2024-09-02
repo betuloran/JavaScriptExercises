8621819fffab4eadcc4c85c81065c5c18a153342
unknown <betuloran2@gmail.com>
2024-09-02 23:09:15 +0300

    file added

--- udemyCourse/part4/diziİçindeObjeTanımlama/main.js
+++ udemyCourse/part4/diziİçindeObjeTanımlama/main.js
@@ -3,27 +3,29 @@ let ogrenci = {
     yas: 29,
     email: 'cnboz@hotmail.com',
     sinif: 12,
-    dersler: ['matematik', 'fizik', 'kimya'],
+    dersler: [
+        { isim: 'matematik', puan: 90 },
+        { isim: 'fizik', puan: 80 },
+        { isim: 'kimya', puan: 85 },],
 
-    login() {
-        console.log('Öğrenci giriş yaptı');
-    },
+    // login() {
+    //     console.log('Öğrenci giriş yaptı');
+    // },
 
-    logout() {
-        console.log('Öğrenci giriş yaptı');
-    },
+    // logout() {
+    //     console.log('Öğrenci giriş yaptı');
+    // },
 
     printLessons() {
         console.log(this.dersler); //(3) ['matematik', 'fizik', 'kimya']
         // console.log(this); //object: bu fonk.nun objectte yani nesnede bulunduğunu anlatır. 
         this.dersler.forEach(ders => {
-            console.log(ders)
+            console.log(ders.isim, ders.puan);
         });
-        //matematik
-        //  fizik
-        //  kimya
     }
-
+    // matematik 90
+    //  fizik 80
+    //  kimya 85
 };
 // console.log(this); //çıktı: window 
 ogrenci.printLessons();
