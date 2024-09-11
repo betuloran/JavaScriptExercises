a46d61ac6b41b77e31ff328489885656c049ac2f
unknown <betuloran2@gmail.com>
2024-09-11 19:41:14 +0300

    file updated

--- udemyCourse/part5/classEklemeKaldırma/main.js
+++ udemyCourse/part5/classEklemeKaldırma/main.js
@@ -1,4 +1,18 @@
-const icerik=document.querySelector('p'); //ilk p etiketine sahip olanı seçtik
+const icerik = document.querySelector('p'); //ilk p etiketine sahip olanı seçtik
 console.log(icerik.classList);
 icerik.classList.add('dom')
 icerik.classList.remove('error')
+
+//ikinci kısım
+const pDegeri = document.querySelectorAll('p');
+
+pDegeri.forEach(a => {
+    if (a.textContent.includes('error')) //eğer seçilen p etiketlerinde error kelimesi varsa 
+    {
+        a.classList.add('error'); //error class ını ekler
+    }
+    if (a.textContent.includes('success')) 
+    {
+        a.classList.add('success');
+    }
+});
