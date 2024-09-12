49c443e2d7f8fc64c59e8ec1ac04184452a5b881
unknown <betuloran2@gmail.com>
2024-09-12 19:54:14 +0300

    file updated

--- udemyCourse/part5/eventBubbling-eventDelegation/main.js
+++ udemyCourse/part5/eventBubbling-eventDelegation/main.js
@@ -10,11 +10,12 @@ button.addEventListener('click', () => {
 
 // -------------------------------------------------------------
 
+//Yeni eklenen öğeler (li), ul öğesine ekleniyor ve tıklama olayı ul'un olay dinleyicisi tarafından işleniyor:
 const ul = document.querySelector('ul');
 ul.addEventListener('click', e => {
     console.log(e.target);
     if (e.target.tagName === 'LI') {
-        e.target.remove();
+        e.target.remove(); //Eğer tıklanan öğe bir li ise, bu öğe kaldırılıyor.Eklenen li öğeleri bu olay dinleyicisine yakalanıyor ve başarıyla siliniyor.
     }
 })
 
@@ -22,7 +23,9 @@ ul.addEventListener('click', e => {
 const liElemanlari = document.querySelectorAll('li');
 liElemanlari.forEach(eleman => {
     eleman.addEventListener('click', e => {
-        e.stopPropagation();
+        e.stopPropagation(); // olayın dıştaki (parent) öğelere yayılmasını engelliyor.
         console.log(e.target);
     })
 })
+
+//stopPropagation : mevcut li öğelerine tıkladığınızda, tıklama olayı ul'a ulaşamıyor ve bu yüzden ul'un tıklama dinleyicisi çalışmıyor. Sonuç olarak, bu öğeler kaldırılmıyor.
