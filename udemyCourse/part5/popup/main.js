0498b0d0e62e8b89d3b4364531f96b8e370a668c
unknown <betuloran2@gmail.com>
2024-10-17 20:26:58 +0300

    form validation added

--- udemyCourse/part5/popup/main.js
+++ udemyCourse/part5/popup/main.js
@@ -1,7 +1,20 @@
-﻿const buton = document.querySelector('button');
+﻿const buton = document.querySelector('.login');
 const maninPopup = document.querySelector('.main-popup');
 const close = document.querySelector('.popup-close');
 
+const form = document.querySelector('.signupForm');
+const message = document.querySelector('.message');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const username = form.username.value;
+    const usernamePattern = /^[a-z]{6,10}$/;
+
+    if(usernamePattern.test(username)) {
+        message.textContent='başarılı!';
+    } else {
+        message.textContent = 'lütfen tüm harfleri küçük ve 6 ile 12 karakter arasında giriniz.'
+    }
+})
 
 
 //tıkla butonuna tıklayınca popup açılır
