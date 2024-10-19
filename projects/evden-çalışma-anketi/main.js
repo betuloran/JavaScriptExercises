47f2c84d2e707f7f03b23a6b96a1c2a2ef333473
unknown <betuloran2@gmail.com>
2024-10-19 20:05:57 +0300

    setInterval feature added

--- projects/evden-çalışma-anketi/main.js
+++ projects/evden-çalışma-anketi/main.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['E', 'E', 'E', 'E'];
 const form = document.querySelector('.questions-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -8,10 +9,37 @@ form.addEventListener('submit', e => {
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
     userAnswers.forEach((answer, index) => {
-        if (answer === correctAnswers[index])   {
+        if (answer === correctAnswers[index]) {
             score += 25;
         }
 
     })
-    console.log(score);
+    result.classList.remove('d-none');
+
+    let puan = 0;
+    const bastir = setInterval(() => { 
+        result.querySelector('span').textContent = `${puan}%`;
+        if (puan == score) {
+            clearInterval(bastir);
+        }else{
+            puan++;
+        }
+    }, 10);
 })
+
+// setTimeout(() => { //1 kez çalışır
+//     console.log('a');
+// }, 2000);
+
+// setInterval(() => { //sürekli çalışır
+//     console.log('a');
+// }, 1000);
+
+// let i=0;
+// const bastir= setInterval(() => {
+//     console.log('a');
+//     i++;
+//     if(i==4){
+//         clearInterval(bastir);
+//     }
+// }, 1000);
