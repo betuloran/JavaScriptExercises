00ecf8eb4583cc737f06d7caba1d810aa963a1f4
unknown <betuloran2@gmail.com>
2024-10-20 12:52:03 +0300

    notes added

--- projects/evden-çalışma-anketi/main.js
+++ projects/evden-çalışma-anketi/main.js
@@ -1,31 +1,31 @@
-const correctAnswers = ['E', 'E', 'E', 'E'];
-const form = document.querySelector('.questions-form');
-const result = document.querySelector('.result');
+const correctAnswers = ['E', 'E', 'E', 'E']; // Doğru yanıtlar dizisi
+const form = document.querySelector('.questions-form'); // Formu seçiyoruz
+const result = document.querySelector('.result'); //Sonucun görüneceği elementi seçiyoruz
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+    e.preventDefault(); // Sayfanın yenilenmesini engeller  
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]; //Kullanıcının verdiği yanıtlar dizi olarak alınır.   
 
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswers[index]) {
-            score += 25;
+            score += 25;//Kullanıcı yanıtları ve doğru yanıtlar tek tek karşılaştırılır ve her doğru cevap için 25 puan eklenir.
         }
+    });
 
-    })
-    result.classList.remove('d-none');
+    result.classList.remove('d-none'); //HTML'de başlangıçta gizlenen sonuç alanını görünür hale getirir
 
     let puan = 0;
     const bastir = setInterval(() => { 
-        result.querySelector('span').textContent = `${puan}%`;
+        result.querySelector('span').textContent = `${puan}%`; //sonucu günceller
         if (puan == score) {
-            clearInterval(bastir);
+            clearInterval(bastir);//Her 10 milisaniyede bir puanı 1 artırarak sonucu yavaşça ekrana basar. Animasyon, puan score ile eşleştiğinde durur.
         }else{
             puan++;
         }
     }, 10);
-})
+});
 
 // setTimeout(() => { //1 kez çalışır
 //     console.log('a');
