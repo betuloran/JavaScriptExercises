16e763ccf97b9b651aeca0a8ab9c73685f91020b
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-10-27 17:22:54 +0300

    json added

--- udemyCourse/part11/async-programming/main.js
+++ udemyCourse/part11/async-programming/main.js
@@ -1,13 +1,22 @@
-const request= new XMLHttpRequest();
+const getTodos = (callback)=>{
+    const request= new XMLHttpRequest();
 
-request.addEventListener('readystatechange',()=>{
-    // console.log(request,request.readyState);
-if(request.readyState===4 && request.status==200){
-    console.log(request.responseText);
-}else if(request.readyState===4 ){
-    console.log("başarısız istek! cevap alınamadı..");
-}
-})
+    request.addEventListener('readystatechange',()=>{
+        // console.log(request,request.readyState);
+    if(request.readyState===4 && request.status==200){
+        // console.log(request.responseText);
+        const data=JSON.parse(request.responseText);
+        callback(undefined,data);
+    }else if(request.readyState===4 ){
+        // console.log("başarısız istek! cevap alınamadı..");
+        callback("başarısız istek! cevap alınamadı..",undefined );
+    }
+    });
+    
+    request.open('GET','https://jsonplaceholder.typicode.com/todos ');
+    request.send();
+};
 
-request.open('GET','https://jsonplaceholder.typicode.com/todosss');
-request.send();
+getTodos((err,data)=>{
+console.log(err,data)
+});
