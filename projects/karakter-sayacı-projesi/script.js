0a49011040f3333a19164194c0fe112027cb52e9
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-11-01 09:46:58 +0300

    javascript codes complated

--- projects/karakter-sayacı-projesi/script.js
+++ projects/karakter-sayacı-projesi/script.js
@@ -5,8 +5,8 @@ const remainingCounter=document.getElementById('remainingCounter');
 textarea.addEventListener('keyup',updateCounter);
 
 function updateCounter(){
-totalCounter.textContent=textarea.value.length;
-remainingCounter.textContent=textarea.getAttribute('maxlength') -  textarea.value.length;
+totalCounter.textContent=textarea.value.length; //textarea içindeki mevcut karakter sayısını gösterir.
+remainingCounter.textContent=textarea.getAttribute('maxlength') -  textarea.value.length; //Maksimum karakter sayısı (maxlength) ile mevcut karakter sayısının farkını hesaplar ve kalan karakter sayısını gösterir.
 }
 
 updateCounter();
