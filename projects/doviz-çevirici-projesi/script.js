e7981017b3252f9907e1d1d2f9e5d1a33bcf6c06
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-11-03 17:16:54 +0300

    explanations added

--- projects/doviz-çevirici-projesi/script.js
+++ projects/doviz-çevirici-projesi/script.js
@@ -1,10 +1,10 @@
 //üzerinde işlem yapacağımız tüm işlevleri çağırıyoruz:
 const currencyFirst=document.getElementById('currencyFirst')
 const currencySecond=document.getElementById('currencySecond')
-const count=document.getElementById('count')
-const equal=document.getElementById('equal')
+const count=document.getElementById('count') //count: Kullanıcının çevirmek istediği miktarı girdiği alan.
+const equal=document.getElementById('equal') //equal: Çevrim sonucunda çıkan değerin gösterildiği alan.
 const exchangeRate=document.querySelector('.exchangeRate') // veya const exchangeRate=document.getElementById('exchangeRate')
-
+//exchangeRate: İki para birimi arasındaki güncel döviz kurunu göstermek için kullanılan alan.
 updateRate();
 
 function updateRate() {
@@ -14,14 +14,12 @@ function updateRate() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      const rate = data.conversion_rates[currencySecond.value];
-
+      const rate = data.conversion_rates[currencySecond.value]; // currencyFirst para biriminden currencySecond para birimine olan döviz kuru alınır.
       exchangeRate.textContent = `1 ${currencyFirst.value} = ${rate} ${currencySecond.value}`;
-
       equal.textContent = (count.value * rate).toFixed(2);
     });
 }
-
+//Olay - Etkinlik Dinleyicileri:
 currencyFirst.addEventListener('change', updateRate);
 currencySecond.addEventListener('change', updateRate);
 count.addEventListener('input', updateRate);
